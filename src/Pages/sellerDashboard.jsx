6f91217c9380ee83206fda3d32e530ecbc03bb6b
Tamil182006 <tamilvanddan350@gmail.com>
2025-06-11 19:31:33 +0530

    Added Seller Dashboard page

---
+++ src/Pages/sellerDashboard.jsx
@@ -0,0 +1,9 @@
+// src/components/SellerDashboard.jsx
+export default function sellerDashboard() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome Seller!</h1>
+      <p>This is the Seller Dashboard page.</p>
+    </div>
+  );
+}
