d90801081b21a9954399b212ea32809550a3b40c
Tamil182006 <tamilvanddan350@gmail.com>
2025-06-12 19:47:19 +0530

    Added seller page and listing logic alon with backend workin!!

--- src/Pages/sellerDashboard.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/SellerDashboard.jsx
-export default function sellerDashboard() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Welcome Seller!</h1>
-      <p>This is the Seller Dashboard page.</p>
-    </div>
-  );
-}
